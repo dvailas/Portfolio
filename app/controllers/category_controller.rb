8b9b7600993ad18b8ad330482900f3e929304da3
Ubuntu <vagrant@Base-Box>
2018-12-21 22:01:12 +0000

    Added Category Partial, fixed a majority of CSS issues that were present

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -6,6 +6,8 @@ class CategoryController < ApplicationController
 
   def show
     @categories = Category.find(params[:id])
+    @posts = Post.where("category_id = ?", params[:id])
+    @id = params[:id]
   end
 
 end
