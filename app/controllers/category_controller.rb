ba3f82439f51055a609ed1b12065caa963cd2dc3
Dylan Vailas <dzastre98@academic.rrc.ca>
2019-02-18 22:49:29 -0600

    Updated Index Css, added footer, new fonts, new sections

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,9 +1,5 @@
 class CategoryController < ApplicationController
 
-  def index
-    @categories = Category.order(:name)
-  end
-
   def show
     @categories = Category.find(params[:id])
     @posts = Post.where("category_id = ?", params[:id])
