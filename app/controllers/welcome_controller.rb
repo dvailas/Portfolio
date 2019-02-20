a10ebfe7b5cec9e24e13d4bed50f234471163fdc
Ubuntu <vagrant@Base-Box>
2019-02-20 17:11:37 +0000

    Finished html for show category

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-      @posts = Post.order(:created_at)
+      @category = Category.where("id = 1 or parent_id = 1")
+      @posts = Post.order(:created_at).where("category_id NOT IN (" + @category.ids.join(",") + ")")
+      @picturePosts = Post.where("category_id IN (" + @category.ids.join(",") + ")")
   end
 end
