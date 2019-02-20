ef8c9db354709f158f84dc924b7cdb7d71189370
Ubuntu <vagrant@Base-Box>
2019-02-20 14:11:03 +0000

    Added Posts and Pictures subcategories to work with _category

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,10 +1,18 @@
 class CategoryController < ApplicationController
 
   def show
-    @categories = Category.where("parent_id >= ?", params[:id])
-    @posts = Post.where("category_id IN (" + @categories.ids.join(",") + ")")
+    @categoryClicked =  Category.find(params[:id])
+    if @categoryClicked.parent_id == nil
+      @categories = Category.where("parent_id = ?", params[:id])
+      @posts = Post.where("category_id IN (" + @categories.ids.join(",") + ")")
+      @count = 0
+    else
+      @categories = @categoryClicked
+      @posts = Post.where("category_id = " + @categories.id.to_s())
+      @count = -1
+    end
     @id = params[:id]
-    @count = 0
+
   end
 
 end
